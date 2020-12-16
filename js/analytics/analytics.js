c7dbbb15fb293161ac02c40bee8af3885e489707
andreazomoza <aszomoza@gmail.com>
2020-12-16 17:08:13 -0300

    id google analytics 4

--- js/analytics/analytics.js
+++ js/analytics/analytics.js
@@ -1,7 +1,6 @@
-$.getScript("https://www.googletagmanager.com/gtag/js?id=XX-NNNNNNNNN-N", function( data, textStatus, jqxhr ) {
-	window.dataLayer = window.dataLayer || [];			
-	function gtag(){dataLayer.push(arguments);}
+$.getScript("https://www.googletagmanager.com/gtag/js?id=G-8HSYJ79EPQ", function( data, textStatus, jqxhr ) {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'XX-NNNNNNNNN-N');	
+    gtag('config', 'G-8HSYJ79EPQ');
 });
-	
