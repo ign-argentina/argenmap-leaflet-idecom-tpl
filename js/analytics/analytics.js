2d45bc6e89e5608a33b79e638040c1c0978b3246
andreazomoza <aszomoza@gmail.com>
2021-03-08 10:41:32 -0300

    Template actualizado con nueva estructura

--- js/analytics/analytics.js
+++
@@ -1,6 +0,0 @@
-$.getScript("https://www.googletagmanager.com/gtag/js?id=G-8HSYJ79EPQ", function( data, textStatus, jqxhr ) {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-8HSYJ79EPQ');
-});
